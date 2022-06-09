51c134cd60ec25a25995a3720333c202df28fc88
Brandon Lehmann <brandonlehmann@gmail.com>
2022-06-09 11:51:29 -0400

    Reduce gas usage in Random library by de-duplicating keccak call(s)

--- contracts/libraries/Random.sol
+++ contracts/libraries/Random.sol
@@ -5,38 +5,6 @@ pragma solidity ^0.8.10;
 
 library Random {
     /**
-     * @dev Generates a random value within the range of the value
-     */
-    function randomize(uint256 value) internal view returns (uint256) {
-        return randomizeWithEntropy(value, getEntropy(), value);
-    }
-
-    /**
-     * @dev Generates a random value within the range of the specified maximum value
-     */
-    function randomize(uint256 value, uint256 maximumValue) internal view returns (uint256) {
-        return randomizeWithEntropy(value, getEntropy(), maximumValue);
-    }
-
-    /**
-     * @dev Generates a random value within the range of the value using the supplied entropy
-     */
-    function randomizeWithEntropy(uint256 value, bytes32 entropy) internal view returns (uint256) {
-        return randomizeWithEntropy(value, entropy, value);
-    }
-
-    /**
-     * @dev Generates a random value within the range of the specified maximum value using the supplied entropy
-     */
-    function randomizeWithEntropy(
-        uint256 value,
-        bytes32 entropy,
-        uint256 maximumValue
-    ) internal view returns (uint256) {
-        return _getEntropy(value, entropy) % maximumValue;
-    }
-
-    /**
      * @dev Generates simple on-chain entropy
      */
     function getEntropy() internal view returns (bytes32) {
@@ -55,9 +23,56 @@ library Random {
     }
 
     /**
-     * @dev Generates simple on-chain entropy that is seeded with the supplied entropy
+     * @dev Generates a random value within the range of the value
+     */
+    function randomize(uint256 value) internal view returns (uint256) {
+        return _getEntropy(value, 0x0) % value;
+    }
+
+    /**
+     * @dev Generates a random value within the range of the specified maximum value
+     */
+    function randomize(uint256 value, uint256 maximumValue) internal view returns (uint256) {
+        return _getEntropy(value, 0x0) % maximumValue;
+    }
+
+    /**
+     * @dev Generates a random value within the range of the value using the supplied entropy
+     */
+    function randomizeWithEntropy(uint256 value, bytes32 entropy) internal view returns (uint256) {
+        return _getEntropy(value, entropy) % value;
+    }
+
+    /**
+     * @dev Generates a random value within the range of the specified maximum value using the supplied entropy
+     */
+    function randomizeWithEntropy(
+        uint256 value,
+        bytes32 entropy,
+        uint256 maximumValue
+    ) internal view returns (uint256) {
+        return _getEntropy(value, entropy) % maximumValue;
+    }
+
+    /**
+     * @dev Generates simple on-chain entropy that is salted with the supplied entropy
      */
     function _getEntropy(uint256 value, bytes32 entropy) private view returns (uint256) {
-        return uint256(keccak256(abi.encodePacked(getEntropy(), value, entropy)));
+        return
+            uint256(
+                keccak256(
+                    abi.encodePacked(
+                        address(this),
+                        msg.sender,
+                        block.coinbase,
+                        block.number,
+                        block.timestamp,
+                        block.difficulty,
+                        msg.data,
+                        value,
+                        entropy
+                    )
+                )
+            );
     }
 }
